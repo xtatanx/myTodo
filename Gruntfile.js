1a9a6addcee73e154fd881acf5041272e68991b4
xtatanx <jhonnatanhxc@gmail.com>
2014-08-03 12:54:38 -0500

    Configured basic grunt file, and updated package.json

---
+++ Gruntfile.js
@@ -0,0 +1,69 @@
+module.exports = function(grunt){
+
+    grunt.initConfig({
+
+        watch: {
+
+            stylesheets: {
+                files: ['public/stylus/*.styl'],
+                tasks: ['stylus', 'autoprefixer'],
+                options: {
+                    spawn: false
+                }
+            },
+
+            scripts: {
+                files: ['public/javascripts/*.js'],
+                tasks: ['jslint']
+            },
+
+            livereload: {
+                options: {
+                    livereload:true
+                },
+                files: ['public/**/*', 'views/*.ejs']
+            }
+        },
+
+        stylus: {
+            compile: {
+                files: {
+                    'public/stylesheets/main.css': 'public/stylus/main.styl'
+                }
+            }
+        },
+
+        jslint: {
+            src: ['public/javascripts/*.js'],
+            directives: {
+                browser: true,
+                predef: [
+                    'jQuery'
+                ]
+            }        
+        },
+
+        autoprefixer: {
+            options: {
+                browsers: ['last 2 version']
+            },
+
+            allFiles: {
+                files: {
+                    'public/stylesheets/main.css' : 'public/stylesheets/main.css'
+                }
+            }
+        }
+
+    });
+
+    // Load grunt modules
+    grunt.loadNpmTasks('grunt-autoprefixer');
+    grunt.loadNpmTasks('grunt-contrib-stylus');
+    grunt.loadNpmTasks('grunt-jslint');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    // Register tasks
+
+    grunt.registerTask('observer', ['watch']);
+
+};
