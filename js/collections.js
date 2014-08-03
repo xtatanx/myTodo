7cb4763f87102b3a2cd6a766b0e18bc17ed054b2
xtatanx <jhonnatanhxc@gmail.com>
2014-08-03 10:40:21 -0500

    Created a express app project

--- js/collections.js
+++
@@ -1,7 +0,0 @@
-var app = window.app || {};
-
-app.Tasks = Backbone.Firebase.Collection.extend({
-    model: app.Task,
-    firebase: 'https://the-todo-app.firebaseio.com/users/' + app.authClient.userId() + '/todos'
-});
-
